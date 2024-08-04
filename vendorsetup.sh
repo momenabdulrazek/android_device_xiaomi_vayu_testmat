f5a77d9e25c68a7a3cee2c96a1a437d445f19a08
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-08-04 09:32:20 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,11 @@ if [ -d "vendor/gms" ]; then
     rm -rf "vendor/gms"
 fi
 
-git clone https://gitlab.com/PixelOS-Devices/vendor_gms.git -b fourteen --depth=1 vendor/gms
+# Gcam
+rm -rf vendor/mgc
+git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
+
+# Dolby
+rm -rf vendor/dolby
+git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
+
