510c9a704a6215971eef5196173fd2d0e0988628
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-08-04 09:24:40 +0300

    AlphaDroid Build Time !

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -1,58 +1,55 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2020-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Inherit common products
+
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit device configurations
+# Inherit from surya device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common AOSP configurations
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Feature Flags
-TARGET_USES_MIUI_CAMERA := true
-TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT := true
 
-# Matrixx
-MATRIXX_BUILD_TYPE := Unofficial
-MATRIXX_MAINTAINER := momenabdulrazekali
-MATRIXX_CHIPSET := SM8150
-MATRIXX_BATTERY := 5160mah
-MATRIXX_DISPLAY := 1080x2400
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := momenabdulrazekali
 
-# Device identifier
-PRODUCT_BRAND := POCO
-PRODUCT_DEVICE := vayu
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_vayu
-
-PRODUCT_SYSTEM_NAME := vayu_global
-PRODUCT_SYSTEM_DEVICE := vayu
-
-# GMS
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_GAPPS_ARCH := arm64
-
-#Bootanim
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Gapps
-WITH_GMS := true
-BUILD_GOOGLE_CONTACTS := true
-BUILD_GOOGLE_DIALER := true
-BUILD_GOOGLE_MESSAGE := true
-
-# Exclude Redundants 
+# Device config
+TARGET_ENABLE_BLUR := true
 TARGET_EXCLUDES_AUDIOFX := true
-TARGET_EXCLUDES_VIA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 13 TKQ1.221013.002 V14.0.3.0.TJUMIXM release-keys" \
-    PRODUCT_MODEL=M2102J20SG
+# Build package
+TARGET_BUILD_PACKAGE := 3
+WITH_GMS := true
+
+# Stock Launcher
+TARGET_LAUNCHER := 1
+
+# GAPPS
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+
+PRODUCT_NAME := lineage_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 13 TKQ1.221013.002 V14.0.3.0.TJUMIXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
